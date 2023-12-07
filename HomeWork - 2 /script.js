57d087f1d1f67d098040fa3b6bb207feb4b1ffde
dushkodonchev <dusko.doncev@yahoo.com>
2023-12-07 10:06:30 +0100

    HomeWork-2

---
+++ HomeWork - 2 /script.js
@@ -0,0 +1,51 @@
+let year = prompt("Please enter your year when you were born: ");
+
+let chineseZodiac = (year-4)%12;
+
+if (chineseZodiac === 0){
+    console.log("In the Chinese Zodiac you are: Rat.");
+}
+
+else if(chineseZodiac === 1){
+     console.log("In the Chinese Zodiac you are: Ox.");
+}
+
+else if(chineseZodiac === 2){
+    console.log("In the Chinese Zodiac you are: Tiger.");
+}
+
+else if(chineseZodiac === 3){
+    console.log("In the Chinese Zodiac you are: Rabbit.");
+}
+
+else if(chineseZodiac === 4){
+    console.log("In the Chinese Zodiac you are: Dragon.");
+}
+
+else if(chineseZodiac === 5){
+    console.log("In the Chinese Zodiac you are: Snake.");
+}
+
+else if(chineseZodiac === 6){
+    console.log("In the Chinese Zodiac you are: Horse");
+}
+
+else if(chineseZodiac === 7){
+    console.log("In the Chinese Zodiac you are: Goat");
+}
+
+else if(chineseZodiac === 8){
+    console.log("In the Chinese Zodiac you are: Horse.");
+}
+
+else if(chineseZodiac === 9){
+    console.log("In the Chinese Zodiac you are: Rooster.");
+}
+
+else if(chineseZodiac === 10){
+    console.log("In the Chinese Zodiac you are: Dog.");
+}
+
+else if(chineseZodiac === 11){
+    console.log("In the Chinese Zodiac you are: Pig.");
+}
